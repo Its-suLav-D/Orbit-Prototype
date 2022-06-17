611009ddd81195d7996f217b2f9eafce82ce7554
Jeremy Busch <jcbusch98@gmail.com>
2022-06-16 19:50:19 -0600

    added a little to test velocity

---
+++ testVelocity.cpp
@@ -0,0 +1,111 @@
+#include "velocity.h"
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+/*******************************
+ * TEST Velocity
+ * The unit test for the Velocity class
+ ********************************/
+
+class TestValocity
+{
+public:
+	void run()
+	{
+		startAtZeroAccelerateX();
+		startAtTenTenAccelerateX();
+		startAtZeroAccelerateXAndY();
+		startAtZeroXDecceleration();
+		startAtTenTenXDecceleration();
+		decelerateXAndY();
+	}
+
+
+private:
+	void startAtZeroAccelerateX()
+	{
+		//setup
+		Velocity vel;
+		vel.setDx(0.0);
+		vel.setDy(0.0);
+
+		//exercise
+		// ?
+		//verify
+		assert(vel.getDx() == 1.0);
+		assert(vel.getDy() == 0.0);
+	} //teardown
+
+	void startAtTenTenAccelerateX()
+	{
+		//setup
+		Velocity vel;
+		vel.setDx(10.0);
+		vel.setDy(10.0);
+
+		//exercise
+		// ?
+		//verify
+		assert(vel.getDx() == 20.0);
+		assert(vel.getDy() == 10.0);
+	} //teardown
+
+	void startAtZeroAccelerateXAndY()
+	{
+		//setup
+		Velocity vel;
+		vel.setDx(0.0);
+		vel.setDy(0.0);
+
+		//exercise
+		// ?
+		//verify
+		assert(vel.getDx() == 10.0);
+		assert(vel.getDy() == 10.0);
+	} //teardown
+
+	void startAtZeroXDecceleration()
+	{
+		//setup
+		Velocity vel;
+		vel.setDx(0.0);
+		vel.setDy(0.0);
+
+		//exercise
+		// ?
+		//verify
+		assert(vel.getDx() == -1.0);
+		assert(vel.getDy() == 0.0);
+	} //teardown
+
+	void startAtTenTenXDecceleration()
+	{
+		//setup
+		Velocity vel;
+		vel.setDx(10.0);
+		vel.setDy(10.0);
+
+		//exercise
+		// ?
+		//verify
+		assert(vel.getDx() == 0.0);
+		assert(vel.getDy() == 10.0);
+	} //teardown
+
+	void decelerateXAndY()
+	{
+		//setup
+		Velocity vel;
+		vel.setDx(0.0);
+		vel.setDy(0.0);
+
+		//exercise
+		// ?
+		//verify
+		assert(vel.getDx() == -10.0);
+		assert(vel.getDy() == -10.0);
+	} //teardown
+
+};
